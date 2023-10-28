8e67e049a39c1666714bc1dfaada142b2d8730ae
Shauda Islam <shauda.islam@mail.utoronto.ca>
2023-10-28 17:48:32 -0400

    Completed steps required for Lab 5 from https://github.com/mjhea0/flaskr-tdd

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 from pathlib import Path
 
@@ -76,6 +75,7 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
